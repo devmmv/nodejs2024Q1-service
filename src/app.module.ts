a0c8a5f334265f85aa5a8661afe8a939617b9c13
Mikhail <nodeprog@gmail.com>
2024-03-26 01:53:27 +0300

    feat: add docker files

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ArtitModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { TrackModule } from './track/track.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { FavoritesModule } from './favorites/favorites.module';
     AlbumModule,
     TrackModule,
     FavoritesModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
